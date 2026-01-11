17e12317678124b3efea44a355f2c3de3cabeba5
atlili <1989575022@qq.com>
2026-01-11 12:25:46 +0800

    Increase the flow response

--- tests/common.rs
+++ tests/common.rs
@@ -3,9 +3,22 @@ use langchainrust::llms::{OpenAIConfig, LLM};
 
 pub fn create_test_llm() -> LLM {
     let config = OpenAIConfig {
-        api_key: "".to_string(),
+        api_key: "sk-l0YYMX65mCYRlTJYH0ptf4BFpqJwm8Xo9Z5IMqSZD0yOafl6".to_string(),
         base_url: "https://api.openai-proxy.org/v1".to_string(),
         model: "gpt-3.5-turbo".to_string(),
+        streaming: false,
+    };
+    LLM::new(config)
+}
+
+
+
+pub fn create_test_llm_straming() -> LLM {
+    let config = OpenAIConfig {
+        api_key: "sk-l0YYMX65mCYRlTJYH0ptf4BFpqJwm8Xo9Z5IMqSZD0yOafl6".to_string(),
+        base_url: "https://api.openai-proxy.org/v1".to_string(),
+        model: "gpt-3.5-turbo".to_string(),
+        streaming: false,
     };
     LLM::new(config)
 }
@@ -13,3 +26,23 @@ pub fn create_test_llm() -> LLM {
 
 
 
+pub fn create_test_llm_Config() -> OpenAIConfig {
+    let config = OpenAIConfig {
+        api_key: "sk-l0YYMX65mCYRlTJYH0ptf4BFpqJwm8Xo9Z5IMqSZD0yOafl6".to_string(),
+        base_url: "https://api.openai-proxy.org/v1".to_string(),
+        model: "gpt-3.5-turbo".to_string(),
+        streaming: false,
+    };
+    return config;
+}
+
+
+pub fn create_test_llm_config_streaming() -> OpenAIConfig {
+    let config = OpenAIConfig {
+        api_key: "sk-l0YYMX65mCYRlTJYH0ptf4BFpqJwm8Xo9Z5IMqSZD0yOafl6".to_string(),
+        base_url: "https://api.openai-proxy.org/v1".to_string(),
+        model: "gpt-3.5-turbo".to_string(),
+        streaming: true,
+    };
+    return config;
+}
